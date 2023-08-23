80d1a24d79ecea80fcb318da132f594acc707580
Turing158 <15818961209@163.com>
2023-08-23 15:49:59 +0800

    管理面板更新，更新主题修改页面

---
+++ src/main/resources/templates/administer/js/Markdown.js
@@ -0,0 +1,5 @@
+function transformToMd(){
+    let htmlContend = document.querySelector('textarea').value;
+    let mark = marked.parse(htmlContend,{ breaks: true });
+    document.getElementById("contentMd").innerHTML = mark;
+}
---
+++ src/main/resources/templates/administer/js/edit.js
@@ -0,0 +1,113 @@
+let show = Vue.createApp({
+    data(){
+        return {
+            flag:false
+        }
+    }
+}).mount('.editTd');
+function h(num){
+    let select = window.getSelection();
+    let str = '';
+    for(let i = 0;i < num;i++){
+        str += '#';
+    }
+    str += ' '+select.toString();
+    replaceText(str);
+}
+function b(){
+    let select = window.getSelection();
+    let str = '**'+select.toString()+'**';
+    replaceText(str);
+}
+function i(){
+    let select = window.getSelection();
+    let str = '*'+select.toString()+'*';
+    replaceText(str);
+}
+function code(){
+    let select = window.getSelection();
+    let str = '```\n'+select.toString()+'\n```';
+    replaceText(str);
+}
+function line(){
+    let str = '\n* * *\n';
+    replaceText(str);
+}
+function del(){
+    let select = window.getSelection();
+    let str = '~~'+select.toString()+'~~';
+    replaceText(str);
+}
+function a(){
+    let select = window.getSelection();
+    let str = '';
+    if(select.toString().length == 0){
+        str = '[请输入](href)';
+    }
+    else{
+        str = '['+select.toString()+'](href)';
+    }
+    replaceText(str);
+}
+function blockquote(){
+    let select = window.getSelection();
+    let str = '> '+select.toString();
+    replaceText(str);
+}
+function mark(){
+    let select = window.getSelection();
+    let str = '`'+select.toString()+'`';
+    replaceText(str);
+}
+function center(){
+    let select = window.getSelection();
+    let str = '<center>'+select.toString()+'</center>';
+    replaceText(str);
+}
+function color(c){
+    let select = window.getSelection();
+    let str = '<font color="'+c+'">'+select.toString()+'</font>';
+    replaceText(str);
+}
+function fontFace(f){
+    let select = window.getSelection();
+    let str = '<font face="'+f+'">'+select.toString()+'</font>';
+    replaceText(str);
+}
+function fontSize(s){
+    let select = window.getSelection();
+    let str = '<font size="'+s+'">'+select.toString()+'</font>';
+    replaceText(str);
+}
+function table(){
+
+}
+function replaceText(str){
+    let text = document.querySelector('textarea');
+    let start = text.selectionStart;
+    let end = text.selectionEnd;
+    let value = text.value;
+    let newValue = value.substring(0,start) + str + value.substring(end);
+    text.value = newValue;
+}
+let date = Date().toString().split(' ');
+let month = checkMonth(date[1]);
+let day = date[2];
+let year = date[3];
+document.querySelector('.date').innerHTML = year+'-'+month+'-'+day;
+function checkMonth(month){
+    switch(month){
+        case 'Jan': return '01';
+        case 'Feb': return '02';
+        case 'Mar': return '03';
+        case 'Apr': return '04';
+        case 'May': return '05';
+        case 'Jun': return '06';
+        case 'Jul': return '07';
+        case 'Aug': return '08';
+        case 'Sep': return '09';
+        case 'Oct': return '10';
+        case 'Nov': return '11';
+        case 'Dec': return '12';
+    }
+}
