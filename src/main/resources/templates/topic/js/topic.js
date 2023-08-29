2245fca778dce0b891b5f35e47b791fda337b96e
Turing158 <15818961209@163.com>
2023-08-29 14:57:56 +0800

    修复了帖子页面评论区变形及js的问题

--- src/main/resources/templates/topic/js/topic.js
+++ src/main/resources/templates/topic/js/topic.js
@@ -22,5 +22,19 @@ function like(num){
             windowsContent.innerHTML = "踩过啦踩过啦！你是有多讨厌这篇！";
             break;
     }
+}
+function showAll(num){
+    window.location.href = '/topic/html?html='+num;
+}
 
+function page(page_num,last_page,user,html) {
+    if (page_num <= last_page && page_num >= 1) {
+        if (user === null) {
+            window.location.href = "/topic/html?html="+html+"&page=" + page_num;
+        } else {
+            window.location.href = "/topic/html?html="+html+"&user=" + user + "&page=" + page_num;
+        }
+    } else {
+        alert("求求别翻了，好不T_T");
+    }
 }
