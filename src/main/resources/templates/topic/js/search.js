5b065e254ccd4d00480fd3a534c6216d40a91fff
Turing158 <15818961209@163.com>
2023-10-05 15:02:50 +0800

    优化了搜索界面，搜索出的关键字变红，且将搜索出的内容里的html标签去除，能看到更多内容

--- src/main/resources/templates/topic/js/search.js
+++ src/main/resources/templates/topic/js/search.js
@@ -1,3 +1,19 @@
+let ketContent = document.querySelectorAll(".keyContent");
+let key = document.querySelector(".key").value;
+for (let i = 0; i < ketContent.length; i++) {
+    let el = ketContent[i].textContent;
+    let els = el.split(key);
+    let newEl = "";
+    for (let j = 0; j < els.length; j++) {
+        let content = els[j] + "<em>" + key + "</em>"
+        if(j === els.length - 1){
+            content = els[j];
+        }
+        newEl = newEl + content;
+    }
+    ketContent[i].innerHTML = newEl;
+}
+
 function page(page_num,last_page,search) {
     if (page_num <= last_page && page_num >= 1) {
         window.location.href = "/topic/search?search="+search+"&page=" + page_num;
